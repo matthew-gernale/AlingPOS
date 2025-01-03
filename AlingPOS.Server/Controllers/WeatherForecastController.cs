41d24057610dc999eafc4b126ac67c19cda88e8a
matthew-gernale <dev.mattgernale@gmail.com>
2025-01-03 18:54:54 +0800

    Project Initialization
    - [x] Created ASP.Net Web API blazor webassembly project.
    - [x] Configured the project dependencies.
    - [x] Configured the project structure.

---
+++ AlingPOS.Server/Controllers/WeatherForecastController.cs
@@ -0,0 +1,26 @@
+
+namespace AlingPOS.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        [HttpGet("get-weather-forecast")]
+        public ActionResult<List<WeatherForecast>> GetWeatherForecast()
+        {
+            var weather = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToList();
+            return Ok(weather);
+        }
+    }
+}
---
+++ AlingPOS.Server/Program.cs
@@ -0,0 +1,36 @@
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+// Add services to the container.
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+builder.Services.AddSignalR();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+
+app.UseHttpsRedirection();
+
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapRazorPages();
+app.MapControllers();
+app.MapFallbackToFile("index.html");
+
+app.Run();
+
